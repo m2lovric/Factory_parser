a004a533475cd951ae3b0c31ed3a5c1a243bf8af
Matteo <m2lovric@gmail.com>
2021-04-12 00:23:11 +0200

    Test update

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
 (async() => {
-  console.log("nešto");
   let text = "";
   let objectsArr = [];
   const regEx = /([{}])/g;
 
-  await fetch('primjer2.txt')
+  await fetch('primjer1.txt')
     .then(res => {
       return res.text();
     })
     .then(data => {
       text = data;
     })
-  
-  console.log(text);
 
   const parts = text.split(regEx);
 
-  //const filteredParts = parts.filter(el => !regEx.test(el));
+  //removing first and last empty element
+  parts[0] === "" ? parts.shift() : '';
+  parts[parts.length - 1] === "" ? parts.splice(-1, 1) : '';
 
   //check if first part of string is text
   if (parts[0] !== "{") {
@@ -24,38 +23,50 @@
     parts.shift();
   }
 
-  console.log(parts);
+  //spliting elements with attributes
+  const elWithAttributes = (parts, num) => {
+    const elWithAtt = parts[num + 1];
+
+    let elements = elWithAtt.split('=');
+    const attributes = [elements[1]];
+
+    let leftSide = elements[0].split(RegExp(/\n| /));
+    const type = leftSide[0];
+    attributes.unshift(leftSide[1]);    
+
+    const obj = {
+      type,
+      attributes: {}
+    };
+
+    for (let i = 0; i < attributes.length; i+=2) {
+      let num = i;
+      let value = attributes[num + 1];
+      value = value[value.length - 1] === "/" ? value.slice(0, value.length - 1) : value;
+      value = value.slice(1, value.length - 1);
+      obj.attributes[attributes[num]] = value;
+      
+    }
+
+    objectsArr.push(obj);
+  }
 
   for (let i = 0; i < parts.length; i++) {
     let num = i;
     if (parts[num] === "{" && parts[num + 2] === "}") {
       if (parts[num + 1].endsWith("/")) {
-        const selfClosing = parts[num + 1];
-        const selfClosingArr = selfClosing.split(/([\n=])/).filter(el => !RegExp(/([\n=])/).test(el));      
-        const type = selfClosingArr[0];
-        selfClosingArr.shift();
-
-        const obj = {
-          type,
-          attributes: {}
-        };
-
-        for (let i = 0; i < selfClosingArr.length; i+=2) {
-          let num = i;
-          obj.attributes[selfClosingArr[num]] = selfClosingArr[num + 1];
-        }
-
-        objectsArr.push(obj);
-      } else if (parts[num + 1].startsWith("/")) { 
-        objectsArr.push({ type: "text", content: parts[num + 3] });
+        elWithAttributes(parts, num);
+      } else if (parts[num + 1].startsWith("/")) {
+        parts[num + 3] === undefined ? '' : objectsArr.push({ type: "text", content: parts[num + 3] });
       } else {
-        objectsArr.push({ type: parts[num + 1], content: parts[num + 3] });
+        if (parts[num + 1].includes("=")) {
+          elWithAttributes(parts, num);
+        } else {
+          objectsArr.push({ type: parts[num + 1], content: parts[num + 3] });
+        }
       }
-    }
-    
+    }    
   }
 
   console.log(objectsArr);
-  console.log(parts);
-
 })();
